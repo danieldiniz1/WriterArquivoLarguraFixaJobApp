194c767499be09aa15dfee18a35309b1a36b47eb
Daniel Alexandre Diniz <daniel.alexandrediniz@emeal.nttdata.com>
2023-03-15 16:58:56 -0300

    forma de formatação particular de arquivo

--- src/main/java/br/com/training/WriterArquivoLarguraFixaJob/writer/ArquivoLarguraFixaWriterConfig.java
+++ src/main/java/br/com/training/WriterArquivoLarguraFixaJob/writer/ArquivoLarguraFixaWriterConfig.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.LineAggregator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.PathResource;
@@ -17,11 +18,23 @@ public class ArquivoLarguraFixaWriterConfig {
     @Bean
     public FlatFileItemWriter<Cliente> arquivoLarguraFixaWriterFormatadoArquivoExterno(){
         return new FlatFileItemWriterBuilder<Cliente>()
-                .name("arquivoLarguraFixaWriter")
-                .resource(new PathResource("files/Clientes-Saida.txt"))
-                .delimited()
-                .delimiter(";")
-                .names(new String[]{"nome","sobrenome","idade","email"})
+                .name("arquivoLarguraFixaWriterFormatadoArquivoExterno")
+                .resource(new PathResource("files/Clientes-Saida-Cabecalho.txt"))
+                .lineAggregator(lineAggregator())
                 .build();
+    }
+
+    private LineAggregator<Cliente> lineAggregator() {
+        return new LineAggregator<Cliente>() {
+            @Override
+            public String aggregate(Cliente cliente) {
+                String cabecalho = "este é o cabeçalho do arquivo";
+
+
+                String formato = String.format("[%d] %s - %s\n",Integer.getInteger(cliente.getIdade()),cliente.getNome(),cliente.getSobrenome());
+
+                return cabecalho + formato;
+            }
+        };
     }
 }
